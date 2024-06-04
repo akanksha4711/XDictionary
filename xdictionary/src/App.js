07d15f44a6c1327621842060b362424145b5c58b
Akanksha Sharma <akku.sharma97@gmail.com>
2024-06-04 10:19:21 +0530

    implemented dictionary

--- xdictionary/src/App.js
+++ xdictionary/src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
+const dictionary = [
+
+    { word: "React", meaning: "A JavaScript library for building user interfaces." },
+
+    { word: "Component", meaning: "A reusable building block in React." },
+
+    { word: "State", meaning: "An object that stores data for a component." }
+
+]
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [result, setResult] = useState('');
+  const handleClick = () => {
+    const answer = dictionary.find(item => item.word.toLowerCase() === query.toLowerCase());
+    if(!answer) setResult("Word not found in the dictionary.")
+    else setResult(answer.meaning);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type='text' value={query} onChange={(e) => setQuery(e.target.value)} />
+      <button onClick={handleClick}>Search</button>
+      <p>{result}</p>
     </div>
   );
 }
